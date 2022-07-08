4cb173885303afef8c0e4a473278756c45a3c815
King Diaw <kingdiawehsut@gmailcom>
2022-07-08 22:15:55 +0800

    uses <> for include

--- examples/autoconnectWithOnDemand/autoconnectWithOnDemand.ino
+++ examples/autoconnectWithOnDemand/autoconnectWithOnDemand.ino
@@ -1,5 +1,5 @@
 //(1)-Inlude Library
-#include "ESP32_WiFiManager.h"
+#include <ESP32_WiFiManager.h>  //https://github.com/kingdiaw/ESP32_WiFiManager
 
 ESP32_WiFiManager wm("ESP32Wifi-AP",2,15); //WiFi AP = "ESP32Wifi-AP", LED at Pin 2, Trigger at Pin 15
 
@@ -10,7 +10,6 @@ void setup() {
   wm.begin();
 }
 
-//====================== LOOP ===========================
 void loop() {
   // put your main code here, to run repeatedly:
   wm.running();
