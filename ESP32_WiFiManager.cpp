7b96c7624411ba8700631733ecb4ccbeb16cee31
User <User@PTSSJKE-KING.poliperlis.edu.my>
2022-07-08 17:28:59 +0800

    initial version library ESP32_WiFiManager

---
+++ ESP32_WiFiManager.cpp
@@ -0,0 +1,70 @@
+#include "Arduino.h"
+#include <WiFiManager.h>
+#include "ESP32_WiFiManager.h"
+
+WiFiManager _wm;
+
+ESP32_WiFiManager::ESP32_WiFiManager(const char* name_AP, int pinLed , int pinTrigger )
+{
+  pinMode(pinLed, OUTPUT);
+  pinMode (pinTrigger, INPUT_PULLUP);
+  _pinLed = pinLed;
+  _pinTrigger = pinTrigger;
+  _name_AP = name_AP;
+}
+
+bool ESP32_WiFiManager::isAutoConnect() {
+  _wm.setConfigPortalBlocking(false);
+  _wm.setConfigPortalTimeout(_timeout);
+  if (_wm.autoConnect(_name_AP)) {
+    Serial.println("connected...yeey :)");
+    _isConfigPortal = false;
+  }
+  else {
+    Serial.println("Configportal running");
+    _isConfigPortal = true;
+  }
+  return !_isConfigPortal;
+}
+
+void ESP32_WiFiManager::blinkStatus(int interval) {
+  static uint32_t __tick;
+  if (__tick < millis()) {
+    __tick = millis() + interval;
+    digitalWrite (_pinLed, digitalRead(_pinLed) ^ 1);
+  }
+}
+
+void ESP32_WiFiManager::running() {
+  static uint16_t __interval_ms;
+  _wm.process();
+
+  if (WiFi.status() == WL_CONNECTED) {
+    __interval_ms = _medium;
+    _isConfigPortal = false;
+  }
+  else if (WiFi.status() != WL_CONNECTED) {
+    __interval_ms = _slow;
+  }
+  
+  if (_isConfigPortal) {
+    __interval_ms = _fast;
+  }
+
+  if (digitalRead (_pinTrigger) == LOW) {
+    //reset settings - for testing
+    _wm.resetSettings();
+
+    // set configportal timeout
+    _wm.setConfigPortalTimeout(_timeout);
+
+    if (!_wm.startConfigPortal(_name_AP)) {
+      Serial.println("failed to connect and hit timeout");
+      delay(3000);
+      //reset and try again, or maybe put it to deep sleep
+      ESP.restart();
+      delay(5000);
+    }
+  }
+  blinkStatus (__interval_ms);
+}
---
+++ ESP32_WiFiManager.h
@@ -0,0 +1,29 @@
+/*
+  ESP32_WiFiManager.h Library for autoconnect WiFi or setup
+  using ConfigPortal
+  Modifed from  https://github.com/tzapu/WiFiManager
+*/
+#ifndef ESP32_WiFiManager_h
+#define ESP32_WiFiManager_h
+
+#include "Arduino.h"
+#include <WiFiManager.h> // https://github.com/tzapu/WiFiManager
+
+class ESP32_WiFiManager
+{
+  public:
+    ESP32_WiFiManager(const char* name_AP, int pinLed = 2, int pinTrigger = 15);
+    bool isAutoConnect();
+    void blinkStatus(int interval);
+    void running();
+  private:
+    int _pinLed;
+    int _pinTrigger;
+    bool _isConfigPortal;
+    const char* _name_AP;
+    const int _timeout = 120;
+    const int _fast = 50;
+    const int _medium = 250;
+    const int _slow = 2500;
+};
+#endif
